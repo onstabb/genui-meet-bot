1dc89af6b37434676762910a1d2208c440ff9625
Vladyslav <65114035+onstabb@users.noreply.github.com>
2020-08-28 11:11:39 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
-TOKEN = os.environ.get('TOKEN') if os.environ.get('TOKEN') is not None else '1198394902:AAFSbNt4G8hR40XSdJsFrB03k14inEA1_5g'
-USER_AGENT = os.environ.get('USER_AGENT') if os.environ.get('USER_AGENT') is not None else 'GenuiMeetBot'
-URL = os.environ.get('DATABASE_URL') if os.environ.get('DATABASE_URL') is not None else "postgres://postgres:12345@localhost:5432/postgres"
-URI = os.environ.get('MONGODB_URI') if os.environ.get('MONGODB_URI') is not None else 'mongodb://heroku_jchdx9gk:j8k71nvnmtpeb80t935q596org@ds221339.mlab.com:21339/heroku_jchdx9gk'
+TOKEN = os.environ.get('TOKEN') 
+USER_AGENT = os.environ.get('USER_AGENT')
+URL = os.environ.get('DATABASE_URL') 
+URI = os.environ.get('MONGODB_URI') 
 
-admins = {"main": 182694754}
+admins = {"main": 1}
 
 
 class Mongo:
